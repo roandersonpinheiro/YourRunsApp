4ee8491557b293dc296e68c45a379580e876eb8b
roandersonpinheiro <roandersonfelipepinheiro@gmail.com>
2022-09-13 16:14:43 -0300

    :sparkles: Created navigation in app

--- app/src/main/java/com/example/yourrunsapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/yourrunsapp/ui/MainActivity.kt
@@ -2,8 +2,12 @@ package com.example.yourrunsapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.yourrunsapp.R
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 @AndroidEntryPoint
@@ -11,5 +15,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+
+        bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
+
+        navHostFragment.findNavController()
+            .addOnDestinationChangedListener { _, destination, _ ->
+                when(destination.id) {
+                    R.id.settingsFragment, R.id.runFragment, R.id.statisticsFragment ->
+                        bottomNavigationView.visibility = View.VISIBLE
+                    else -> bottomNavigationView.visibility = View.GONE
+                }
+            }
     }
 }
--- app/src/main/java/com/example/yourrunsapp/ui/fragments/SetupFragment.kt
+++ app/src/main/java/com/example/yourrunsapp/ui/fragments/SetupFragment.kt
@@ -1,12 +1,23 @@
 package com.example.yourrunsapp.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.example.yourrunsapp.R
 import com.example.yourrunsapp.ui.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_setup.*
 
 @AndroidEntryPoint
-class SetupFragment:Fragment(R.layout.fragment_setup) {
+class SetupFragment : Fragment(R.layout.fragment_setup) {
     private val viewModel: MainViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        tvContinue.setOnClickListener {
+            findNavController().navigate(R.id.action_setupFragment_to_runFragment)
+        }
+    }
 }
