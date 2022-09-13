7da27aca1510d28e24004623645e12ea073ba31e
roandersonpinheiro <roandersonfelipepinheiro@gmail.com>
2022-09-13 14:32:28 -0300

    :sparkles: Created Converters and Created database and dependency injection configured with Dagger

--- app/src/main/java/com/example/yourrunsapp/db/RunningDatabase.kt
+++ app/src/main/java/com/example/yourrunsapp/db/RunningDatabase.kt
@@ -1,4 +1,16 @@
 package com.example.yourrunsapp.db
 
-class RunningDataBase {
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+
+@Database(
+    entities = [Run::class],
+    version = 1
+)
+@TypeConverters(Converters::class)
+abstract class RunningDatabase : RoomDatabase() {
+
+    abstract fun getRunDao(): RunDAO
 }
--- app/src/main/java/com/example/yourrunsapp/other/Constants.kt
+++ app/src/main/java/com/example/yourrunsapp/other/Constants.kt
@@ -1,4 +1,5 @@
 package com.example.yourrunsapp.other
 
 object Constants {
+    const val RUNNING_DATABASE_NAME = "running_db"
 }
