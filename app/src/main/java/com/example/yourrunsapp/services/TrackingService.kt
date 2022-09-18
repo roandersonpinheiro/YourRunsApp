685c3a57c1ec8de465794815b921f5d15553765d
roandersonpinheiro <roandersonfelipepinheiro@gmail.com>
2022-09-18 12:15:15 -0300

    :sparkles: feature get location user background

--- app/src/main/java/com/example/yourrunsapp/services/TrackingService.kt
+++ app/src/main/java/com/example/yourrunsapp/services/TrackingService.kt
@@ -1,39 +1,130 @@
 package com.example.yourrunsapp.services
 
+import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.NotificationManager.IMPORTANCE_LOW
 import android.app.PendingIntent
-import android.app.PendingIntent.FLAG_IMMUTABLE
-import android.app.PendingIntent.FLAG_UPDATE_CURRENT
 import android.content.Context
 import android.content.Intent
+import android.location.Location
 import android.os.Build
+import android.os.Looper
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import androidx.lifecycle.LifecycleService
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import com.example.yourrunsapp.R
 import com.example.yourrunsapp.other.Constants.ACTION_PAUSE_SERVICE
 import com.example.yourrunsapp.other.Constants.ACTION_SHOW_TRACKING_FRAGMENT
 import com.example.yourrunsapp.other.Constants.ACTION_START_OR_RESUME_SERVICE
 import com.example.yourrunsapp.other.Constants.ACTION_STOP_SERVICE
+import com.example.yourrunsapp.other.Constants.FASTEST_LOCATION_INTERVAL
+import com.example.yourrunsapp.other.Constants.LOCATION_UPDATE_INTERVAL
 import com.example.yourrunsapp.other.Constants.NOTIFICATION_CHANNEL_ID
 import com.example.yourrunsapp.other.Constants.NOTIFICATION_CHANNEL_NAME
 import com.example.yourrunsapp.other.Constants.NOTIFICATION_ID
+import com.example.yourrunsapp.other.TrackUtility
 import com.example.yourrunsapp.ui.MainActivity
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.Priority.PRIORITY_HIGH_ACCURACY
+import com.google.android.gms.maps.model.LatLng
 import timber.log.Timber
+import javax.inject.Inject
+
+typealias Polyline = MutableList<LatLng>
+typealias Polylines = MutableList<Polyline>
 
 class TrackingService : LifecycleService() {
 
     var isFirstRun = true
+
+    @Inject
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    companion object {
+        val isTracking = MutableLiveData<Boolean>()
+        val pathPoints = MutableLiveData<Polylines>()
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        postInitialValue()
+        fusedLocationProviderClient = FusedLocationProviderClient(this)
+        isTracking.observe(this, Observer {
+            updateLocationTracking(it)
+        })
+    }
+
+    private fun postInitialValue() {
+        isTracking.postValue(true)
+        pathPoints.postValue(mutableListOf())
+    }
+
+    private fun addEmptyPolyline() = pathPoints.value?.apply {
+        add(mutableListOf())
+        pathPoints.postValue(this)
+    } ?: pathPoints.postValue(mutableListOf(mutableListOf()))
+
+
+    @SuppressLint("MissingPermission")
+    private fun updateLocationTracking(isTracking: Boolean) {
+        if (isTracking) {
+            if (TrackUtility.hasLocationPermissions(this)) {
+                val request = LocationRequest().apply {
+                    interval = LOCATION_UPDATE_INTERVAL
+                    fastestInterval = FASTEST_LOCATION_INTERVAL
+                    priority = PRIORITY_HIGH_ACCURACY
+
+                }
+                fusedLocationProviderClient.requestLocationUpdates(
+                    request,
+                    locationCallback,
+                    Looper.getMainLooper()
+                )
+            } else {
+                fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+            }
+        }
+    }
+
+    val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(result: LocationResult) {
+            super.onLocationResult(result)
+            if (isTracking.value!!) {
+                result?.locations?.let { locations ->
+                    for (location in locations) {
+                        addPathPoint(location)
+                        Timber.d("NEW LOCATION: ${location.latitude}, ${location.longitude}")
+                    }
+                }
+
+            }
+        }
+    }
+
+    private fun addPathPoint(location: Location?) {
+        location?.let {
+            val pos = LatLng(location.latitude, location.longitude)
+            pathPoints.value?.apply {
+                last().add(pos)
+                pathPoints.postValue(this)
+            }
+        }
+    }
+
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         intent?.let {
             when (it.action) {
                 ACTION_START_OR_RESUME_SERVICE -> {
-                    if(isFirstRun){
+                    if (isFirstRun) {
                         startForegroundService()
                         isFirstRun = false
-                    }else{
+                    } else {
                         Timber.d("Resuming service...")
                     }
 
@@ -53,6 +144,9 @@ class TrackingService : LifecycleService() {
     }
 
     private fun startForegroundService() {
+        addEmptyPolyline()
+        isTracking.postValue(true)
+
         val notificationManager =
             getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
@@ -68,17 +162,16 @@ class TrackingService : LifecycleService() {
                 .setContentText("00:00:00")
                 .setContentIntent(getMainAcitivtyPendingIntent())
 
-        startForeground(NOTIFICATION_ID,notificationBuilder.build())
+        startForeground(NOTIFICATION_ID, notificationBuilder.build())
     }
 
 
     private fun getMainAcitivtyPendingIntent() = PendingIntent.getActivity(
         this,
         0,
-        Intent(this,MainActivity::class.java).also {
+        Intent(this, MainActivity::class.java).also {
             it.action = ACTION_SHOW_TRACKING_FRAGMENT
         },
-       // FLAG_UPDATE_CURRENT,
         PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
     )
 
