a5799d82429ddde4366e638c31b36b5dfd4f474a
roandersonpinheiro <roandersonfelipepinheiro@gmail.com>
2022-09-25 17:52:02 -0300

    Sorting runs

--- app/src/main/java/com/example/yourrunsapp/ui/fragments/RunFragment.kt
+++ app/src/main/java/com/example/yourrunsapp/ui/fragments/RunFragment.kt
@@ -4,6 +4,7 @@ import android.Manifest
 import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
@@ -12,6 +13,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.yourrunsapp.R
 import com.example.yourrunsapp.adapters.RunAdapter
 import com.example.yourrunsapp.other.Constants.REQUEST_CODE_LOCATION_PERMISSION
+import com.example.yourrunsapp.other.SortType
 import com.example.yourrunsapp.other.TrackUtility
 import com.example.yourrunsapp.ui.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -30,7 +32,35 @@ class RunFragment : Fragment(R.layout.fragment_run), PermissionCallbacks {
         requestPermissions()
         setupRecyclerView()
 
-        viewModel.runsSortedByDate.observe(viewLifecycleOwner, Observer {
+        when (viewModel.sortType) {
+            SortType.DATE -> spFilter.setSelection(0)
+            SortType.RUNNING_TIME -> spFilter.setSelection(1)
+            SortType.DISTANCE -> spFilter.setSelection(2)
+            SortType.AVG_SPEED -> spFilter.setSelection(3)
+            SortType.CALORIES_BURNED -> spFilter.setSelection(4)
+        }
+
+        spFilter.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(p0: AdapterView<*>?) {}
+
+            override fun onItemSelected(
+                adapterView: AdapterView<*>?,
+                view: View?,
+                pos: Int,
+                id: Long
+            ) {
+                when (pos) {
+                    0 -> viewModel.sortRuns(SortType.DATE)
+                    1 -> viewModel.sortRuns(SortType.RUNNING_TIME)
+                    2 -> viewModel.sortRuns(SortType.DISTANCE)
+                    3 -> viewModel.sortRuns(SortType.AVG_SPEED)
+                    4 -> viewModel.sortRuns(SortType.CALORIES_BURNED)
+                }
+            }
+        }
+
+
+        viewModel.runs.observe(viewLifecycleOwner, Observer {
             runAdapter.submitList(it)
         })
 
@@ -40,7 +70,7 @@ class RunFragment : Fragment(R.layout.fragment_run), PermissionCallbacks {
     }
 
     private fun setupRecyclerView() = rvRuns.apply {
-        runAdapter  = RunAdapter()
+        runAdapter = RunAdapter()
         adapter = runAdapter
         layoutManager = LinearLayoutManager(requireContext())
     }
